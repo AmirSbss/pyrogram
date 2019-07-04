d3a575462a912bd7f4b0af8a472b5ca209a2e376
Amirh. Safavi <co.amirh@gmail.com>
2019-07-04 20:41:53 +0430

    Update storage.py

--- pyrogram/client/storage/storage.py
+++ pyrogram/client/storage/storage.py
@@ -16,79 +16,70 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
-from async_property import async_property
-
 
 class Storage:
     def __init__(self, name: str):
         self.name = name
 
-    async def open(self):
+    def open(self):
         raise NotImplementedError
 
-    async def save(self):
+    def save(self):
         raise NotImplementedError
 
-    async def close(self):
+    def close(self):
         raise NotImplementedError
 
-    async def update_peers(self, peers):
+    def update_peers(self, peers):
         raise NotImplementedError
 
-    async def get_peer_by_id(self, peer_id):
+    def get_peer_by_id(self, peer_id):
         raise NotImplementedError
 
-    async def get_peer_by_username(self, username):
+    def get_peer_by_username(self, username):
         raise NotImplementedError
 
-    async def get_peer_by_phone_number(self, phone_number):
+    def get_peer_by_phone_number(self, phone_number):
         raise NotImplementedError
 
-    async def export_session_string(self):
+    def export_session_string(self):
         raise NotImplementedError
 
-    @async_property
-    async def peers_count(self):
+    def get_peers_count(self):
         raise NotImplementedError
 
-    @async_property
-    async def dc_id(self):
+    def get_dc_id(self):
         raise NotImplementedError
 
-    async def set_dc_id(self, value):
+    def set_dc_id(self, value):
         raise NotImplementedError
 
-    @async_property
-    async def test_mode(self):
+    def get_test_mode(self):
         raise NotImplementedError
 
-    async def set_test_mode(self, value):
+    def set_test_mode(self, value):
         raise NotImplementedError
 
-    @async_property
-    async def auth_key(self):
+    def get_auth_key(self):
         raise NotImplementedError
 
-    async def set_auth_key(self, value):
+    def set_auth_key(self, value):
         raise NotImplementedError
 
-    @async_property
-    async def date(self):
+    def get_date(self):
         raise NotImplementedError
 
-    async def set_date(self, value):
+    def set_date(self, value):
         raise NotImplementedError
 
-    @async_property
-    async def user_id(self):
+    def get_user_id(self):
         raise NotImplementedError
 
-    async def set_user_id(self, value):
+    def set_user_id(self, value):
         raise NotImplementedError
 
-    @async_property
-    async def is_bot(self):
+    def get_is_bot(self):
         raise NotImplementedError
 
-    async def set_is_bot(self, value):
+    def set_is_bot(self, value):
         raise NotImplementedError
